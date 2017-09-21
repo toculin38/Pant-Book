bd3b954b85527aa84e3880ac8dd93ec063587a76
AkiraQiu <toculin38@gamil.com>
2017-09-21 16:33:22 +0800

    first commit

---
+++ Assets/ColorArea.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PolygonCollider2D))]
+public class ColorArea : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Patlette.NowColor;
+    }
+
+}
---
+++ Assets/Patlette.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Patlette : MonoBehaviour {
+    public static Color NowColor = Color.white;
+    // Use this for initialization
+    public void OnColorClick(Image image) {
+        NowColor = image.color;
+    }
+}
